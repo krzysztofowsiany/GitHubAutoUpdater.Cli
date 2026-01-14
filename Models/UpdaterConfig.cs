48c0002ced3270f6a4291bf85f45f394de141c19
Krzysztof Owsiany <krzysztof.owsiany@euvic.pl>
2026-01-14 23:14:11 +0100

    move cli to self project

---
+++ Models/UpdaterConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace GitHubAutoUpdater.Cli.Models;
+
+internal class UpdaterConfig
+{
+    public string Repository { get; set; } = string.Empty;
+    public string CurrentVersion { get; set; } = string.Empty;
+}
---
+++ Services/LicenseService.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubAutoUpdater.Cli.Services;
+public class LicenseService
+{
+    public void EnsurePro(string repoVisibility)
+    {
+        if (repoVisibility == "private")
+            throw new Exception("Private repositories require GitHubUpdater Pro");
+    }
+}
